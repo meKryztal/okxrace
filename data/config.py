88a68d137aeb1a05389a08447aaff5f1518ab6f9
Bodya <47853767+meKryztal@users.noreply.github.com>
2024-08-06 14:02:26 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,34 @@
+# api id, hash
+API_ID = 1111111111 # апи
+API_HASH = 'тут апи'
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [10, 30]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# рефка
+REF_CODE = 'linkCode_67665606'
+
+# скипает таски с этим айди, это подкл кошелька и кус
+BLACKLIST = {5,9}
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# задержка между кругом
+BIG_SLEEP = [30000,60000]
+
+# мини задержки
+MINI_SLEEP = [1,3] #[min,max]
+
+# задержка между тасками
+TASK_SLEEP = [2,5] #[min,max]
+
+# задержка между бустами
+BOOST_SLEEP = [1,2] #[min,max]
+
